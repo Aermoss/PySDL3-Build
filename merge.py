4e0ec33d2666ea2c45552dfb72faf124f49c1832
Aermoss <86446300+Aermoss@users.noreply.github.com>
2025-01-24 01:46:21 +0300

    .

--- merge.py
+++ merge.py
@@ -1,17 +1,22 @@
-import sys, os, shutil, platform
+import sys, os, shutil, platform, zipfile
 
 def main(argv):
     repos = ["SDL", "SDL_image", "SDL_mixer", "SDL_ttf", "SDL_rtf", "SDL_net"]
     binaries = ["SDL3", "SDL3_image", "SDL3_mixer", "SDL3_ttf", "SDL3_rtf", "SDL3_net"]
-    system, workDir = platform.system(), os.getcwd()
+    system, workDir, outDir = platform.system(), os.getcwd(), "artifacts"
 
-    if not os.path.exists(os.path.join(workDir, "artifacts")):
-        os.mkdir(os.path.join(workDir, "artifacts"))
+    if not os.path.exists(os.path.join(workDir, outDir)):
+        os.mkdir(os.path.join(workDir, outDir))
 
     for index, repo in enumerate(repos):
         file = {"Windows": "{}.dll" , "Linux": "lib{}.so", "Darwin": "lib{}.dylib"}[system].format(binaries[index])
         path = os.path.join(workDir, repo, "build", *(["Release", file] if system in ["Windows"] else [file]))
-        shutil.copyfile(path, os.path.join(workDir, "artifacts", file))
+        shutil.copyfile(path, os.path.join(workDir, outDir, file))
+
+    with zipfile.ZipFile(f"{outDir}.zip", "w", zipfile.ZIP_DEFLATED) as ref:
+        for root, _, files in os.walk(outDir):
+            for file in files:
+                ref.write(path := os.path.join(root, file), os.path.relpath(path, outDir))
 
     return 0
 
