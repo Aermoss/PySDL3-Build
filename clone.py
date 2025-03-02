635e32404fdb50f27e6e121b0c0d22d678abe965
Aermoss <86446300+Aermoss@users.noreply.github.com>
2025-03-03 02:30:08 +0300

    .

--- clone.py
+++ clone.py
@@ -12,8 +12,8 @@ def main(argv):
         if repo not in os.listdir():
             dev = release is None or not release.split("-")[1].startswith("3")
             print("\33[32m", f"info: cloning '{repo}'", f"with release '{release}'..." if not dev else "without any release...", "\33[0m", sep = "", flush = True)
-            subprocess.run(["git", "clone", f"https://github.com/libsdl-org/{repo}.git", "--recursive"], shell = sdl3.SDL_SYSTEM not in ["Darwin"])
-            if not dev: subprocess.run(["git", "checkout", f"tags/{release}"], cwd = repo, shell = sdl3.SDL_SYSTEM not in ["Darwin"])
+            subprocess.run(["git", "clone", f"https://github.com/libsdl-org/{repo}.git", "--recursive"], shell = sdl3.SDL_SYSTEM in ["Windows"])
+            if not dev: subprocess.run(["git", "checkout", f"tags/{release}"], cwd = repo, shell = sdl3.SDL_SYSTEM in ["Windows"])
 
         else:
             print("\33[35m", f"warning: passing '{repo}' because it already exists...", "\33[0m", sep = "", flush = True)
