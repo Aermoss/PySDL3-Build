b4e71ab8fd4dc53b7e6927a5976a2b7d71f7c3bf
Aermoss <86446300+Aermoss@users.noreply.github.com>
2025-03-03 02:42:41 +0300

    .

--- clone.py
+++ clone.py
@@ -8,6 +8,8 @@ os.environ["SDL_FIND_BINARIES"] = "0"
 import sdl3, asyncio
 
 def main(argv):
+    subprocess.run(["git", "config", "--global", "advice.detachedHead", "false"], shell = sdl3.SDL_SYSTEM in ["Windows"])
+
     for repo, release in asyncio.run(sdl3.SDL_GET_LATEST_RELEASES()).items():
         if repo not in os.listdir():
             print("\33[32m", f"info: cloning '{repo}'", "without any release..." if (dev := release is None or not release.split("-")[1].startswith("3")) else f"with release '{release}'...", "\33[0m", sep = "", flush = True)
