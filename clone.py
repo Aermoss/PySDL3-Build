56faeef104af61c76a7b6f5cd32c8699ece453a9
Aermoss <86446300+Aermoss@users.noreply.github.com>
2025-03-07 04:04:29 +0300

    .

--- clone.py
+++ clone.py
@@ -13,7 +13,7 @@ def main(argv):
     for repo, release in asyncio.run(sdl3.SDL_GET_LATEST_RELEASES()).items():
         if repo not in os.listdir():
             print("\33[32m", f"info: cloning '{repo}'", "without any release..." if (dev := release is None or not release.split("-")[1].startswith("3")) else f"with release '{release}'...", "\33[0m", sep = "", flush = True)
-            subprocess.run(["git", "clone", f"https://github.com/libsdl-org/{repo}.git", "--recursive"] + ([] if dev else ["--branch", release]), shell = sdl3.SDL_SYSTEM in ["Windows"])
+            subprocess.run(["git", "clone", f"https://github.com/libsdl-org/{repo}.git"] + ([] if "old" in os.listdir() else ["--recursive"]) + ([] if dev else ["--branch", release]), shell = sdl3.SDL_SYSTEM in ["Windows"])
 
         else:
             print("\33[35m", f"warning: passing '{repo}' because it already exists...", "\33[0m", sep = "", flush = True)
