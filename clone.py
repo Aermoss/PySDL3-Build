65fd534d52fcf62576cf74611c231f38e8f4f6a4
Aermoss <86446300+Aermoss@users.noreply.github.com>
2025-06-09 19:34:18 +0300

    Update logging system.

--- clone.py
+++ clone.py
@@ -1,5 +1,7 @@
 import sys, os, subprocess
 
+import sdl3.SDL
+
 os.environ["SDL_DOC_GENERATOR"] = "0"
 os.environ["SDL_DISABLE_METADATA"] = "1"
 os.environ["SDL_CHECK_BINARY_VERSION"] = "0"
@@ -13,11 +15,11 @@ def main(argv):
 
     for repo, release in asyncio.run(sdl3.SDL_GET_LATEST_RELEASES()).items():
         if repo not in os.listdir():
-            print("\33[32m", f"info: cloning '{repo}'", "without any release..." if (dev := release is None or not release.split("-")[1].startswith("3")) else f"with release '{release}'...", "\33[0m", sep = "", flush = True)
-            subprocess.run(["git", "clone", f"https://github.com/libsdl-org/{repo}.git"] + ([] if "old" in os.listdir() else ["--recursive"]) + ([] if dev else ["--branch", release]), shell = sdl3.SDL_SYSTEM in ["Windows"])
+            sdl3.SDL_LOGGER.Log(sdl3.SDL_LOGGER.Info, f"Cloning '{repo}' without any release..." if (dev := release is None or not release.split("-")[1].startswith("3")) else f"Cloning '{repo}' with release '{release}'...")
+            subprocess.run(["git", "clone"] + ([] if "old" in os.listdir() else ["--recursive"]) + ([] if dev else ["--branch", release]) + [f"https://github.com/libsdl-org/{repo}.git"], shell = sdl3.SDL_SYSTEM in ["Windows"])
 
         else:
-            print("\33[35m", f"warning: passing '{repo}' because it already exists...", "\33[0m", sep = "", flush = True)
+            sdl3.SDL_LOGGER.Log(sdl3.SDL_LOGGER.Warning, f"Skipping '{repo}' because it already exists...")
 
     return 0
 
